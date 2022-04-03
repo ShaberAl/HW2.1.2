7a37d4ef0af6c7189217cddf737b83bdd97f69b1
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2022-04-03 11:00:41 +0500

    Rounded lights

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = redLightView.frame.size.height / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.height / 2
+        startButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func changeColourButton(_ sender: UIButton) {
+        
+    }
 }
 
